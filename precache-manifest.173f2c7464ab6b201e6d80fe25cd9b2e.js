41a2cf22a32991a3eddb8893f87ee02fc364d16e
Vadim <vadim@iMac-Vadim.Dlink>
2019-04-18 14:44:29 +0300

    Updates

---
+++ precache-manifest.173f2c7464ab6b201e6d80fe25cd9b2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "12215efb2905a93dfe16ac847a0982dd",
+    "url": "/HellYeahOmg/music-tags/static/media/mood.12215efb.svg"
+  },
+  {
+    "revision": "59c79e23ede6a934943cd8d871bc90d5",
+    "url": "/HellYeahOmg/music-tags/static/media/list.59c79e23.svg"
+  },
+  {
+    "revision": "69eeacc1e8e669641b911273fdc1ecbe",
+    "url": "/HellYeahOmg/music-tags/static/media/genre.69eeacc1.svg"
+  },
+  {
+    "revision": "11dd9761570e9c6698731d62ff5b5896",
+    "url": "/HellYeahOmg/music-tags/static/media/arrow.11dd9761.svg"
+  },
+  {
+    "revision": "d03c8fc22314bc189a5f",
+    "url": "/HellYeahOmg/music-tags/static/js/runtime~main.494ac3fb.js"
+  },
+  {
+    "revision": "ae8efd6824577bf417fd",
+    "url": "/HellYeahOmg/music-tags/static/js/main.085bb506.chunk.js"
+  },
+  {
+    "revision": "78e43aa6027a49281d44",
+    "url": "/HellYeahOmg/music-tags/static/js/2.c59ade42.chunk.js"
+  },
+  {
+    "revision": "ae8efd6824577bf417fd",
+    "url": "/HellYeahOmg/music-tags/static/css/main.d8768fe0.chunk.css"
+  },
+  {
+    "revision": "9393f4565f6a5e258ed17d2093c3d7ce",
+    "url": "/HellYeahOmg/music-tags/index.html"
+  }
+];
